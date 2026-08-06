9dbd2a84c0084ff2df7a273297a9c6ed19b18435
José Guilherme Guerra <63473997+guerraguilherme@users.noreply.github.com>
2026-08-05 20:51:03 -0400

    Add sessions service worker

---
+++ sessoes/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME='dataprev-sessoes-v1';
+const CORE=['./','./index.html','./sessions.json','./manifest.webmanifest','../icons/icon-192.png','../icons/icon-512.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin)return;
+  if(event.request.method!=='GET')return;
+  if(url.pathname.endsWith('/sessions.json')){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request)));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+    return response;
+  })));
+});
